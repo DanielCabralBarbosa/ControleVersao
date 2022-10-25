ed3f872536150dbf947143e8ba62834a3f5c8bcc
TgCruzz <87619682+TgCruzz@users.noreply.github.com>
2022-10-25 15:36:04 -0300

    Criação de novas variáveis e atribuição de valores

--- Controle/src/br/com/controle/git/mainClass.java
+++ Controle/src/br/com/controle/git/mainClass.java
@@ -2,12 +2,16 @@ package br.com.controle.git;
 
 public class mainClass {
 	
-	String nome;
+	String nome, profissao, nacionalidade;
 	int idade;
 	
 	
+	
 	public void main(String [] args) {
-		
+			this.nome = "Robert";
+			this.idade = 56;
+			this.profissao = "Mecanico";
+			this.nacionalidade = "Ingles";
 		
 	}
 
